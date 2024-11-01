c5adafcda9111fb05c79d89d52699e2dcb4f3092
brunodifante <134736986+bouulzzz@users.noreply.github.com>
2024-11-01 10:43:44 -0300

    Update Pessoa.java

--- ClientePessoaVeiculo/src/beans/Pessoa.java
+++ ClientePessoaVeiculo/src/beans/Pessoa.java
@@ -1,15 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package beans;
 
 import java.io.Serializable;
 
-/**
- *
- * @author iagov
- */
 public class Pessoa implements Serializable {
     private int id;
     private String nome;
