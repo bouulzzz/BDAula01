d978c9b777cbc656ababe745b5cb3c53442133c7
brunodifante <134736986+bouulzzz@users.noreply.github.com>
2024-11-01 10:44:05 -0300

    Update Veiculo.java

--- ClientePessoaVeiculo/src/beans/Veiculo.java
+++ ClientePessoaVeiculo/src/beans/Veiculo.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package beans;
 
-/**
- *
- * @author iagov
- */
 public class Veiculo {
     private int id;
     private String modelo;
