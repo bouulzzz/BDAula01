05ee68f8dc5c1528bae9f6b3c5679552f704df6a
brunodifante <134736986+bouulzzz@users.noreply.github.com>
2024-11-07 22:23:21 -0300

    Update Javabd01Conexao.java

--- javabd01.conexao/src/javabd01/conexao/Javabd01Conexao.java
+++ javabd01.conexao/src/javabd01/conexao/Javabd01Conexao.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package javabd01.conexao;
 import beans.Pessoa;
 import DAO.PessoaDAO;
@@ -10,14 +6,11 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
-/**
- *
- * @author iagov
- */
+
 public class Javabd01Conexao {
 
   public static void main(String[] args) {
-        int porta = 12345; // Use uma constante para a porta
+        int porta = 12345;
 
         try (ServerSocket servidorSocket = new ServerSocket(porta)) {
             System.out.println("Servidor aguardando conexões na porta " + porta);
